e82b95e4599cff638c623abaf5639cc4fc253f46
pirateces.github.io <pirateces@byui.edu>
2020-10-15 15:49:42 -0600

    adding code

---
+++ js/poundsToKilograms.js
@@ -0,0 +1,13 @@
+/***********
+* Making a program to add two numbers together
+**********/
+function addNumbers() {
+    //Input
+    let pounds = parseFloat(document.getElementById("pounds").value);
+
+    //Process
+    let kilograms = pounds * 0.453592;
+    let kilograms = kilograms.toFixed(1);
+
+    //Output
+    document.getElementById('output').innerHTML = "The conversion is " + kilograms + ".";
