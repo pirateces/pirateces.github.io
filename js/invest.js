fad59822343d7eb2775e105a1ab40dd57b71cc14
pirateces.github.io <pirateces@byui.edu>
2020-11-28 11:04:40 -0700

    filling it in

--- js/invest.js
+++ js/invest.js
@@ -7,10 +7,18 @@ function doFV() {
     let annualRate = parseFloat(document.getElementById("annualRate").value);
     let numOfYears = parseFloat(document.getElementById("noYears").value);
     let periodsPerYear = parseFloat(document.getElementById("periodsPerYear").value);
+    let FV = 0;
 
     //Process
-    
+    function computeFutureValue(amountInvested, annualRate, numOfYears, periodsPerYear)
 
     //Output
     document.getElementById('output').innerHTML = "The conversion is " + kilos + "Kg.";
+}
+function computeFutureValue(a, annualRate, numOfYears, periodsPerYear){
+    //process for the calcuations
+    let r = annualRate/12;
+    let n = numOfYears * periodsPerYear;
+    let f = a * Math.pow((1 + r),n);
+    return f;
 }
