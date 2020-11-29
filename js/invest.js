065fd98954183970dc05f550cff4a33cd1d1243a
pirateces.github.io <pirateces@byui.edu>
2020-11-28 21:11:24 -0700

    minor fixes

--- js/invest.js
+++ js/invest.js
@@ -9,10 +9,11 @@ function doFV() {
     let periodsPerYear = parseFloat(document.getElementById("periodsPerYear").value);
 
     //Process
-    let FV = function computeFutureValue(amountInvested, annualRate, numOfYears, periodsPerYear)
+    let FV = computeFutureValue(amountInvested, annualRate, numOfYears, periodsPerYear);
+    let f = FV.toFixed(2);
 
     //Output
-    document.getElementById('output').innerHTML = FV;
+    document.getElementById('output').innerHTML = '$' + f;
 }
 function computeFutureValue(a, annualRate, numOfYears, periodsPerYear){
     //process for the calcuations
